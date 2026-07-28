4f04f17024642d2d7a5684e9766a0d03ad06b872
HyeonWoo Cho <robert12154937@gmail.com>
2026-07-28 10:15:01 +0900

    fix: change  readOnly->edit ok

--- src/pages/AnalyzePage.jsx
+++ src/pages/AnalyzePage.jsx
@@ -592,7 +592,7 @@ export default function AnalyzePage() {
                       setDiffMode(false);
                     }}
                     spellCheck={false}
-                    readOnly={compareMode || Boolean(filePath)}
+                    readOnly={compareMode}
                   />
                 </div>
               </Card>
